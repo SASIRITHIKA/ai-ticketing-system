d1d6a3bffa0186d704ea53d89a83185bab771a72
sazz@2352 <sasirithikaselvaraj200@gmail.com>
2026-03-19 20:34:14 +0530

    route updated for authorisation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,6 @@ const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, getMe);
+
 
 module.exports = router;
